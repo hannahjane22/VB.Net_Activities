66fd0cd08167760d380f3e129de89a7de698d0d3
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:37:31 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.3/Example 3.1-3d/Example 3.1-3d/Form1.vb
@@ -0,0 +1,20 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim grade As String
+
+        grade = TextBox1.Text
+
+        Select Case grade
+            Case "A"
+                Label1.Text = "High Distinction"
+            Case "B"
+                Label1.Text = "Distinction"
+            Case "C"
+                Label1.Text = "Credit"
+            Case "D"
+                Label1.Text = "Pass"
+            Case "D"
+                Label1.Text = "Fail"
+        End Select
+    End Sub
+End Class
