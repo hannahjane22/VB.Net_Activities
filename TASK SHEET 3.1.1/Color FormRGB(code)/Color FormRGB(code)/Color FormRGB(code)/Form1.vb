b864627201e99aec76561af3f4d49c87c0168fd9
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:16:01 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.1/Color FormRGB(code)/Color FormRGB(code)/Color FormRGB(code)/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim rgb1, rgb2, rgb3 As Integer
+
+        rgb1 = TextBox1.Text
+        rgb2 = TextBox2.Text
+        rgb3 = TextBox3.Text
+
+        Me.BackColor = Color.FromArgb(rgb1, rgb2, rgb3)
+    End Sub
+End Class
