1c7efdf24c6faedd1af04105376bea5efe3234da
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:23:29 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/Example 3.1-2c/Example 3.1-2c/Example 3.1-1c/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Const Pi As Single = 3.142
+        Const Temp As Single = 37
+        Const Score As Single = 100
+    End Sub
+End Class
