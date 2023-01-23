cfefb6e086511d040d15e50006a3abc48a833b37
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:35:04 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/Show Hidden Names/Show Hidden Names/Show Hidden Names/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim name1, name2, name3 As String
+
+        name1 = "John"
+        name2 = "Georges"
+        name3 = "Ali"
+
+        MsgBox("The names are " & name1 & " , " & name2 & " and " & name3)
+    End Sub
+End Class
