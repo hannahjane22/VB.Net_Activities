4d480bd1c1d538ab0e68075b7e3439b5da3de2cb
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:27:29 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/Example 3.1-2e/Example 3.1-2e/Example 3.1-2e/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim a, b, c As Single
+
+        a = TextBox1.Text
+        b = TextBox2.Text
+        c = (a ^ 2 + b ^ 2) ^ (1 / 2)
+
+        Label1.Text = c
+    End Sub
+End Class
