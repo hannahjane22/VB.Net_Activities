6ea72d91c69e3f419fa1b271b15bdb3c78e2ad0e
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:31:52 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/Excercise 3.1-2f/Excercise 3.1-2f/Excercise 3.1-2f/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim height, weight, bmi As Single
+
+        height = TextBox1.Text
+        weight = TextBox2.Text
+        bmi = (weight) / (height ^ 2)
+
+        TextBox3.Text = bmi
+    End Sub
+End Class
