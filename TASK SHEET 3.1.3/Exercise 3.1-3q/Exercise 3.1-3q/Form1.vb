da3128a601500f3485b9123b2addb487e0f12af2
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:45:14 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.3/Exercise 3.1-3q/Exercise 3.1-3q/Form1.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim myPhrase As String
+
+        myPhrase = Microsoft.VisualBasic.InputBox("Enter a phrase")
+        Label1.Text = (Trim(myPhrase))
+    End Sub
+End Class
---
+++ TASK SHEET 3.1.3/Exercise 3.1-3r/Exercise 3.1-3r/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2 As Integer
+
+        num1 = TextBox1.Text
+        num2 = Math.Exp(num1)
+
+        Label1.Text = num2
+    End Sub
+End Class
