eae8c9611501a40437e77598dcd1156dd9639737
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:20:37 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/Example 3.1-1a/Example 3.1-1a/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim password As String
+        Dim yourname As String
+        Dim firstnum As Integer
+        Dim secondnum As Integer
+        Dim total As Integer
+        Dim doDate As Date
+    End Sub
+End Class
