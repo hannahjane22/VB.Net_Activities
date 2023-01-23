58d62c144e7f8794f1c16f0fff9bf7544432049a
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:40:25 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.3/Example 3.1-3v/Example 3.1-3v/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2 As Single
+
+        num1 = TextBox1.Text
+        num2 = Math.Round(num1, 2)
+
+        Label1.Text = num2
+    End Sub
+End Class
