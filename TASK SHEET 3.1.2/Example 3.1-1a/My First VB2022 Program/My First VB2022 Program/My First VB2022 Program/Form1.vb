95b63df2396903955e0fa0136670bdd988331d5a
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:34:19 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/My First VB2022 Program/My First VB2022 Program/My First VB2022 Program/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "My First VB2022 Program"
+        Me.ForeColor = Color.ForestGreen
+        Me.BackColor = Color.Cyan
+
+    End Sub
+End Class
---
+++ TASK SHEET 3.1.2/Example 3.1-1a/My First VB2022 Program/My First VB2022 Program/My First VB2022 Program/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("My First VB2022 Program")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("My First VB2022 Program")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("0ea22065-a5d9-4646-ba64-efc6c0a3b899")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
