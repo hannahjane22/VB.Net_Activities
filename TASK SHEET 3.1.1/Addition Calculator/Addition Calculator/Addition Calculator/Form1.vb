f13193dd6ebe7887df894e7ea3c9cc77f86a7628
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:12:18 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.1/Addition Calculator/Addition Calculator/Addition Calculator/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2, product As Single
+
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        product = num1 + num2
+
+        TextBox3.Text = product
+    End Sub
+End Class
