4081b6d2e5b84362ff7cae36337e3b747fd7213c
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:25:12 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/Example 3.1-2d/Example 3.1-2d/Example 3.1-2d/Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim num1, num2, difference, product, quotient, sum As Single
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        sum = num1 + num2
+        difference = num1 - num2
+        product = num1 * num2
+        quotient = num1 / num2
+
+        Label1.Text = sum
+        Label2.Text = difference
+        Label3.Text = product
+        Label4.Text = quotient
+    End Sub
+End Class
