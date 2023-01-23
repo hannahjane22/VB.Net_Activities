ace11d86bc44b91db2a06d542e7669219331896d
Hannah Evangelista <107899431+hannahjane22@users.noreply.github.com>
2023-01-23 14:35:17 +0800

    Add files via upload

---
+++ TASK SHEET 3.1.2/Example 3.1-1a/String Manipulation/String Manipulation/String Manipulation/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim text1, text2, text3, text4, text5, text6 As String
+
+        text1 = "Welcome"
+        text2 = " to"
+        text3 = " Visual"
+        text4 = " Basic"
+        text5 = " 2022"
+
+        text6 = text1 + text2 + text3 + text4 + text5
+        Label1.Text = text6
+    End Sub
+End Class
